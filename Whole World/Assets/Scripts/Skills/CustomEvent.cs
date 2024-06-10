88e3b2a2dacafb2d75a3b67bd6a4f6288fbe4b18
Thomas Engels <m.darkbasicer@gmail.com>
2024-06-10 12:25:26 +0200

    added custom event manager base

---
+++ Whole World/Assets/Scripts/Skills/CustomEvent.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public sealed class CustomEvent {
+	public enum MessageType {
+		INFO
+	}
+
+	public delegate void MessageCallback(string title, Sprite iconSprite, string message, MessageType mtype);
+	public static event MessageCallback messageCallback;
+
+	public static void SendMessage(string title, Sprite iconSprite, string message, MessageType mtype) {
+		if (messageCallback != null)
+			messageCallback(title, iconSprite, message, mtype);
+	}
+
+	static readonly CustomEvent _instance = new CustomEvent();
+	public static CustomEvent Instance {
+		get {
+			return _instance;
+		}
+	}
+
+	private CustomEvent() {
+
+	}
+}
