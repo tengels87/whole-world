da5f8d3c1974021d2c97eed04f8adbdf9e330d15
Thomas Engels <m.darkbasicer@gmail.com>
2024-08-23 23:16:05 +0200

    added PhysicsDisableOnCollision and IKhands

---
+++ Whole World/Assets/Scripts/DisablePhysicsOnCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisablePhysicsOnCollision : MonoBehaviour {
+    private Rigidbody rb;
+
+    void Start() {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Terrain")) {
+            rb.useGravity = false;
+            rb.isKinematic = true;
+        }
+    }
+}
